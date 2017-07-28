fb968bae1002a985eb3198be9492e0b0f4f771c2
sorrysy1 <sorrysy7@163.com>
2017-07-28 17:24:52 +0800

    Create Singleton.h

---
+++ Singleton.h
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Singleton
+{
+public:
+	static Singleton *GetInstance()
+	{
+		return const_cast <Singleton *>(m_Instance);
+	}
+
+	static void DestoryInstance()
+	{
+		if (m_Instance != NULL )
+		{
+			delete m_Instance;
+			m_Instance = NULL ;
+		}
+	}
+
+	int GetTest()
+	{
+		return m_Test;
+	}
+
+private:
+	Singleton(){ m_Test = 10; }
+	static const Singleton *m_Instance;
+	int m_Test;
+};
+
+const Singleton *Singleton ::m_Instance = new Singleton();
